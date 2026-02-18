6a65b949e54ce20fac451d31b48afa22e22a9c63
mukoko <mukoko@gmail.com>
2026-02-19 02:41:53 +0900

    Configuration settings.py et staticfiles

--- eglise/models.py
+++ eglise/models.py
@@ -7,8 +7,27 @@ class Fonction(models.Model):
 
     def __str__(self):
         return self.nomFonction 
+# ===============================
+# cellule 
+# ===============================
+class Cellule(models.Model):
+    nomCellule = models.CharField(max_length=30) 
+    province   = models.CharField(max_length=30) 
+
+    def __str__(self):
+        return self.nomCellule
+    
     
 # =================================
 # profile 
 # =================================
+class Profil(models.Model):
+    user = models.OneToOneField(User , on_delete = models.CASCADE)
+    fonction = models.ForeignKey(Fonction , on_delete=models.CASCADE ) 
+    celluel  = models.ForeignKey(Cellule , on_delete= models.CASCADE) 
+    phone = models.IntegerField()
 
+
+    def __str__(self):
+        return self.phone 
+    
